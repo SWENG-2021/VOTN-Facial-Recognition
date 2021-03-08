ab8c338b435527643f6f892d4e5e7fa68858a1cc
Conor Loftus <conor.lo.lynch556@gmail.com>
2021-03-08 12:25:03 +0000

    Added feature to specify if detecting faces in still image or video.

---
+++ recognition.py
@@ -0,0 +1,89 @@
+
+"""
+Early Steps (Non anaconda approach)
+1) Make sure Python 3.5+ is installed
+2) Make sure you pip install opencv-python (4.3.0.36 is the version I am using)
+3) Test that the pip install works for opencv (test importing it)
+
+
+Design Steps
+-------------------------------------------------------------------------------
+1) Now need to think about how we get the video into the python program
+2) What kind of processing do we need to do with the video
+3) What do we do with the data generated?
+
+
+How to use:
+-------------------------------------------------------------------------------
+1) If you want to do facial recognition on a still image
+        py recognition.py picture.png i
+
+2) If you want to do facial recognition on a video
+        py recognition.py video.mp4 v
+"""
+
+import cv2, sys
+import pyautogui
+
+# Get user supplied values
+imagePath = sys.argv[1]
+formatSetting = sys.argv[2]     # i = image, v = video
+cascPath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+
+
+def imageDetection(show_results):
+    # Read the image
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+        #flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
+
+    print("Found {0} faces!".format(len(faces)))
+
+    if (show_results):
+        #Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        cv2.imshow("Faces found", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+
+def videoDetection():
+    pass
+
+
+
+
+#Here is the main function
+def main():
+    print("Starting Program")
+
+    if (formatSetting == 'v'):
+        # We are taking in a video
+        videoDetection()
+        print("Finished Video Detection")
+
+    elif (formatSetting == 'i'):
+        # We are taking in an image
+        imageDetection(show_results=True)
+        print("Finished Image Detection")
+
+    else:
+        print("Error: invalid format setting provided. Use 'v' for video or 'i' for image.")
+
+main()
+print("Exiting out of program")
