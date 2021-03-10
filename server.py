c871bd5aed36fd7fdc4936c1c98ea6763f1ec49c
Pavel Petrukhin <petrukhp@tcd.ie>
2021-03-10 14:17:01 +0000

    Update server.py

--- server.py
+++ server.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 def webhook_post():
     print("POST")
     print(request.json)
-    return Response(200)
+    return Response(status=200)
 
 @app.route('/',methods=['GET'])
 def webhook_get():
     print("GET")
     print(request.json)
-
+    return Response(status=200)
