0645d7b745aa32492f3bf3c816c114a76c69dbba
Pavel Petrukhin <petrukhp@tcd.ie>
2021-03-10 12:44:01 +0000

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,5 @@
 sudo apt install python3 -y
 sudo apt install python3-pip -y
 pip3 install flask
-sudo apt-get install nginx
-sudo apt-get install gunicorn3
+sudo apt-get install nginx -y
+sudo apt-get install gunicorn3 -y
