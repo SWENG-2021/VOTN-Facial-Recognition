4fc39a91576925a233b32569ae17c73a70cef7bd
Pavel Petrukhin <petrukhp@tcd.ie>
2021-03-10 13:49:03 +0000

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def webhook():
     print(request.json)
-    return "<h1>Hello world</h1>"
+    return Response(200)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
