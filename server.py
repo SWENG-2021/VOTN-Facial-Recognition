a532cb00d72680acbd7c276fe02ea11d8a1c7a19
Pavel Petrukhin <petrukhp@tcd.ie>
2021-03-10 16:02:00 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,7 @@
 ### webhook listener, using flask
 from flask import Flask, request, Response
-from flask_ngrok import run_with_ngrok 
 
 app = Flask(__name__)
-run_with_ngrok(app) 
-
 
 @app.route('/',methods=['POST'])
 def webhook_post():
@@ -18,5 +15,3 @@ def webhook_get():
     print(request.json)
     return Response(status=200)
 
-if __name__ == "__main__": 
-  app.run()
