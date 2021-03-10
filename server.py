9a6358ed9c33c506a60c7c2d1bee766e17b78969
Pavel Petrukhin <petrukhp@tcd.ie>
2021-03-10 13:54:35 +0000

    Update server.py

--- server.py
+++ server.py
@@ -4,13 +4,13 @@ from flask import Flask, request, Response
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
-def webhook():
+def webhook_post():
     print("POST")
     print(request.json)
     return Response(200)
 
 @app.route('/',methods=['GET'])
-def webhook():
+def webhook_get():
     print("GET")
     print(request.json)
     return Response(200)
