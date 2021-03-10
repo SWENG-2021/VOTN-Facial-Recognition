ab9a6595e392483780d44a9314253d5e834873d8
Pavel Petrukhin <petrukhp@tcd.ie>
2021-03-10 12:35:05 +0000

    Update server.py

--- server.py
+++ server.py
@@ -3,13 +3,10 @@ from flask import Flask, request, Response
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/')
 def webhook():
     print(request.json)
-    if request.method == 'POST':
-        return Response(status=200)
-    else:
-        return Response(status=400)
+    return "<h1>Hello world</h1>"
 
 if __name__ == '__main__':
     app.run()
