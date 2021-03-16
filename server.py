8612215c2866b6750474a929554504b293c857eb
Pavel Petrukhin <petrukhp@tcd.ie>
2021-03-16 15:11:08 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
 ### webhook listener, using flask
 from flask import Flask, request, Response
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/',methods=['POST'])
+@app.route('/webhook',methods=['POST'])
 def webhook_post():
     print("POST")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
     print(request.json)
     return Response(status=200)
 
-@app.route('/',methods=['GET'])
+@app.route('/webhook',methods=['GET'])
 def webhook_get():
     print("GET")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
     print(request.json)
     return Response(status=200)
 
