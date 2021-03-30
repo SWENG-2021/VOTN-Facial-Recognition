13eb54e4f020ab7856d579bb68ebc3ecfc2936fd
Pavel Petrukhin <petrukhp@tcd.ie>
2021-03-30 18:17:26 +0100

    20-> 8 concurrent threads
    Low performance server (our free aws machine) has trouble working with 20, so reduce to 8 and see what happens, might need to reduce more

--- download.py
+++ download.py
@@ -6,7 +6,7 @@ def download(asset_id):
     token = getenv("FRAME_IO_TOKEN")
     client = FrameioClient(token)
     asset_info = client.get_asset(asset_id)
-    filename = client.download(asset_info, "", prefix="", multi_part=True, concurrency=20)
+    filename = client.download(asset_info, "", prefix="", multi_part=True, concurrency=8)
     return filename
 
 
