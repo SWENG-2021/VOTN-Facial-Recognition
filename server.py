0c639c6be51aba417a9f611a1c1a109b9a82ec70
cppavel <petrukhp@tcd.ie>
2021-04-17 13:45:41 +0100

    initial version of pictures to db, testing currently

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 ### webhook listener, using flask
+import shutil
+
 from flask import Flask, request, Response
 from datetime import datetime
 from download import download
@@ -7,8 +9,51 @@ from metadata import  add_metadata
 from new_recognition import loadAllFaces, detectVideoFaces
 import os 
 import threading
-import json
+
 app = Flask(__name__)
+
+
+@app.route('/addpicture', methods=['POST'])
+def addpicture_post():
+    print("adding picture initiated")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+    print(request.json)
+    headers = request.headers
+    timestamp = int(headers["X-Frameio-Request-Timestamp"])
+    signature = headers["X-Frameio-Signature"]
+    print(timestamp)
+    print(signature)
+    print(os.getenv("CUSTOM_ACTIONS_SECRET"))
+    verified = verifyWebhook("v0", timestamp, request.json, signature, os.getenv("SECRET"))
+    print(verified)
+    if verified:
+        asset_id = request.json["resource"]["id"]
+        th = threading.Thread(target=downloadImage, args=(asset_id,), daemon=True)
+        th.start()
+    else:
+        print("unverified request")
+
+    return Response(status=200)
+
+
+def downloadImage(asset_id):
+    filename = download(asset_id)
+
+    print("Succesfully downloaded: " + filename)
+
+    print("Checking that file is an image...")
+
+    directory_name, file_extension = os.path.splitext(filename)
+
+    if ((file_extension != ".jpeg") or (file_extension != ".png")):
+        return
+    else:
+        print("File is a compatible image!")
+        print("Moving image...")
+        os.mkdir(os.path.join(os.getcwd(),"known_faces",directory_name))
+        shutil.move(filename,os.path.join(os.getcwd(),"known_faces",directory_name,filename))
 
 @app.route('/webhook',methods=['POST'])
 def webhook_post():
