d42882f7805075a703fc6cb35cbcebb653818408
Pavel Petrukhin <petrukhp@tcd.ie>
2021-04-19 19:58:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 sudo apt-get update
 sudo apt install ffmpeg 
 sudo apt-get install python3-pip python3-dev nginx
-sudo pip3 install cmake
-sudo pip3 install face_recognition
-sudo pip3 install scikit-build
-sudo pip3 install opencv-python
-sudo pip3 install gunicorn
-sudo pip3 install frameioclient
-sudo pip3 install flask
+sudo pip3 install cmake -vvv
+sudo pip3 install face_recognition -vvv
+sudo pip3 install scikit-build -vvv
+sudo pip3 install opencv-python -vvv
+sudo pip3 install gunicorn -vvv
+sudo pip3 install frameioclient -vvv
+sudo pip3 install flask -vvv
